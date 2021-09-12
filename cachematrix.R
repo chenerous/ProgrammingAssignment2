38c5e85cf51cced733cc880921e3d5f73b2c92a3
Jacqueline Chen <chenerous@gmail.com>
2021-09-11 21:54:05 -0400

    Script to calculate inverse of square matrix complete.  This version only updates some comments in the code.

--- cachematrix.R
+++ cachematrix.R
@@ -15,45 +15,44 @@
 ## 2.  get the value of the matrix 
 ## 3.  set the value of the matrix inverse 
 ## 4.  get the value of the matrix inverse
-
 ## Within the function, the matrix is denoted by x and the inverse by inv
 
 makeCacheMatrix <- function(x = matrix()) {
   inv <- NULL
   
-  ## 'set' the matrix by assigning the matrix to be the input of the function
+  ## 'set' the matrix by assigning the matrix to be the input of the below 
+  ## function
   set <- function(y) {
     x <<- y
     inv <<- NULL
   }
   
-  ## 'get' the matrix by returning matrix
+  ## 'get' the matrix by returning the matrix
   get <- function() x
   
   ## 'set' the inverse by assigning the inverse matrix to be the input of the 
-  ## function
+  ## below function
   setinverse <- function(inverse) inv <<- inverse
   
   ## 'get' the inverse by returning the inverse matrix 
   getinverse <- function() inv
 
-  ## function returns list of functions
+  ## return a list of functions
   list(set = set, get = get,
        setinverse = setinverse,
        getinverse = getinverse)
 }
 
 
-## The following function calculates the inverse of the matrix
-## created with the above function. However, it first checks to see if the
+## cacheSolve calculates the inverse of the matrix created with the 
+## makeCacheMatrix function. However, it first checks to see if the
 ## inverse has already been calculated. If so, it `get`s the inverse from the
-## cache and skips the computation. 
-
-## Within the function, x denotes the list returned by the function above
-## and inv denotes the matrix inverse
-
+## cache and skips the computation. Within the function, x denotes the 
+## list returned by makeCacheMatrix and inv denotes the matrix inverse
+ 
 cacheSolve <- function(x, ...) {
-  ## Return a matrix that is the inverse of 'x'
+  ## Return a matrix that is the inverse of 'x' (where 'x' is the output 
+  ## of makeCacheMatrix)
   
   inv <- x$getinverse() ## check to see if inverse is already calculated
   
